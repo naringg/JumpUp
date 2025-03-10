6315db8b01836782c414e3199e3eb25851317bac
Naring <0224kjh@gmail.com>
2025-03-10 23:17:57 +0900

    [feat]에셋 추가, 포인터를 통한 interactable 정보 상호 작용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
 
     private void Awake()
     {
---
+++ Assets/Scripts/item/ItemData.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Resource,
+    Consumable
+}
+
+public enum ConsumableType
+{
+    Health
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+}
